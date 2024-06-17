9ad06a36af1a7b77d22ede92a63db0f3781df907
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-17 17:26:46 +0200

    Create search.js

---
+++ PicKgr/src/Components/Search/search.js
@@ -0,0 +1,19 @@
+import("./search.css");
+
+export const createSearchInput = (node, placeholder, buttonText, onSearch) => {
+  const searchContainer = document.createElement("div");
+  searchContainer.className = "search-container";
+  node.appendChild(searchContainer);
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.className = "search-input";
+  input.placeholder = placeholder;
+  searchContainer.appendChild(input);
+
+  const button = document.createElement("button");
+  button.className = "search-button";
+  button.innerText = buttonText;
+  button.addEventListener("click", onSearch);
+  searchContainer.appendChild(button);
+};
