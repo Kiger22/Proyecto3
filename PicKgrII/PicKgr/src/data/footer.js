5a09d59c4e998e031fa073e7baec68c331d088a9
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-18 16:45:18 +0200

    Create footer.js

---
+++ PicKgrII/PicKgr/src/data/footer.js
@@ -0,0 +1,14 @@
+export const footerLogo = "./public/assets/Logo2.png"
+
+export const menuFooter = [
+  { text: 'Inicio', href: '#' },
+  { text: 'Sobre Nosotros', href: '#' },
+  { text: 'Servicios', href: '#' },
+  { text: 'Contacto', href: '#' }
+];
+
+export const socialLinks = [
+  { href: 'https://facebook.com', src: "./public/assets/facebook.png" },
+  { href: 'https://twitter.com', src: "./public/assets/twitter.png" },
+  { href: 'https://instagram.com', src: "./public/assets/instagram.png" }
+];
