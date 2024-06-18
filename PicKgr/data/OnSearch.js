d0180632219ed7262e8bd01e49776eebdcf6b55c
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-18 16:42:27 +0200

    Delete OnSearch.js

--- PicKgr/data/OnSearch.js
+++
@@ -1,8 +0,0 @@
-import { callApi } from "../main"
-
-const input = document.querySelector(".search-input");
-const keyword = input.value.trim();
-
-export const OnSearch = () => {
-  callApi(keyword);
-}
