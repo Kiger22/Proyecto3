2da9a66f41418c402db84fb89e75bade4c0772a6
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-18 16:45:21 +0200

    Create header.js

---
+++ PicKgrII/PicKgr/src/data/header.js
@@ -0,0 +1,23 @@
+export const menuItemsHeaderI = [
+  { text: "Hoy", href: '#' },
+  { text: "Explorar", href: '#' },
+];
+
+export const menuItemsHeaderII = [
+  { text: "Info", href: '#' },
+  { text: "Empresa", href: '#' },
+];
+
+export const loginItems = [
+  { text: "HOY", href: '#' },
+  { text: "EXPLORAR", href: '#' },
+];
+
+export const heaterLogo = "./public/assets/LogoSolo.png"
+
+export const placeholder = "Encuentra tu imagen"
+export const buttonText = "Buscar"
+
+const handleSearch = (query) => {
+  alert(`Buscando: ${query}`);
+};
