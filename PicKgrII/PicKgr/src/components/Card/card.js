4bd0ee726aa04b0ba9090b1d095490cd7c289c67
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-18 16:45:01 +0200

    Create card.js

---
+++ PicKgrII/PicKgr/src/components/Card/card.js
@@ -0,0 +1,30 @@
+import("./card.css");
+
+export const createCard = (node, imgSrc, title, description) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  app.appendChild(card);
+
+  const img = document.createElement("img");
+  img.src = imgSrc;
+  card.appendChild(img);
+
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  card.appendChild(cardBody);
+
+  if (title) {
+    const cardTitle = document.createElement("h2");
+    cardTitle.className = "card-title";
+    cardTitle.innerText = title;
+    cardBody.appendChild(cardTitle);
+  }
+
+  if (description) {
+    const cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerText = description;
+    cardBody.appendChild(cardText);
+  }
+
+};
