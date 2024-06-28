c914599e41514fe2e021c6bc41a6bc473dd82244
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 17:03:34 +0200

    Update OnSearch.js

--- PicKgrII/PicKgr/actions/OnSearch.js
+++ PicKgrII/PicKgr/actions/OnSearch.js
@@ -3,37 +3,7 @@ import { callApi } from "../main";
 export const OnSearch = () => {
   //alert(" Buscando");
   const searchInput = document.querySelector(".search-input");
-  //const searcButton = document.querySelector(".search-button");
 
-  //searcButton.addEventListener('click', () => {
   callApi(searchInput.value);
-  //});
 
 }
-
-/* import { appDiv } from "../main"
-import { createCard } from "../src/components/Card/card"
-
-export const OnSearch = (keyword) => {
-
-  const accessKey = 'UdGLeHvGPYod1K-LPYP0HevqnNknec_b1g6Ov_5VbF4'
-  const API_URL = `https://api.unsplash.com/search/photos?query=${keyword}&client_id=${accessKey}`
-
-  fetch(API_URL)
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      res.results.forEach(results => {
-        const card = createCard(appDiv, results.urls.full);
-
-          card.querySelector("img").addEventListener("click", () => {
-            openModal(image.urls.full);
-          });
-
-      });
-    })
-    .catch((error) => {
-      alert("Error al obtener los datos de la API");
-    })
-
-} */
