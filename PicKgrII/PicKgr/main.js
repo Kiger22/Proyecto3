7f3589d3698d2ed0551b685c00897b5430522a52
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 16:56:40 +0200

    Update main.js

--- PicKgrII/PicKgr/main.js
+++ PicKgrII/PicKgr/main.js
@@ -16,13 +16,16 @@ export const callApi = (keyword) => {
   const accessKey = 'UdGLeHvGPYod1K-LPYP0HevqnNknec_b1g6Ov_5VbF4'
   const API_URL = `https://api.unsplash.com/search/photos?query=${keyword}&client_id=${accessKey}`
 
+  appDiv.innerHTML = "";
+
   fetch(API_URL)
     .then((res) => res.json())
     .then((res) => {
       console.log(res);
       res.results.forEach(results => {
-        const card = createCard(appDiv, results.urls.full);
 
+        //const card = createCard(appDiv, results.urls.full);
+        createCard(appDiv, results.urls.full);
         /* card.querySelector("img").addEventListener("click", () => {
           openModal(image.urls.full);
         }); */
