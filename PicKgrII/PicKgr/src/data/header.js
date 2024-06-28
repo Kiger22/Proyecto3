426924a4bc776d03db3defcef81ad05e3cd2774e
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 17:03:41 +0200

    Update header.js

--- PicKgrII/PicKgr/src/data/header.js
+++ PicKgrII/PicKgr/src/data/header.js
@@ -1,6 +1,6 @@
 export const menuItemsHeaderI = [
-  { text: "Hoy", href: '#' },
-  { text: "Explorar", href: '#' },
+  { text: "HOY", href: '#' },
+  { text: "EXPLORAR", href: '#' },
 ];
 
 export const menuItemsHeaderII = [
@@ -8,16 +8,7 @@ export const menuItemsHeaderII = [
   { text: "Empresa", href: '#' },
 ];
 
-export const loginItems = [
-  { text: "HOY", href: '#' },
-  { text: "EXPLORAR", href: '#' },
-];
-
 export const heaterLogo = "./public/assets/LogoSolo.png"
 
 export const placeholder = "Encuentra tu imagen"
 export const buttonText = "Buscar"
-
-const handleSearch = (query) => {
-  alert(`Buscando: ${query}`);
-};
